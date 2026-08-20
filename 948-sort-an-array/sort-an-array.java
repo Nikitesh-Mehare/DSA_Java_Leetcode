7590b21599520db4f9173855fa18b93d3f8d6ec6
Nikitesh Mehare <meharenikitesh@gmail.com>
2026-08-20 11:28:37 +0530

    Time: 36 ms (23.59%) | Memory: 86.8 MB (8.22%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,81 +1,119 @@
 class Solution {
     public int[] sortArray(int[] nums) {
+        
+        return quickSort(nums, 0, nums.length-1);
 
-        mergeSort(nums, 0, nums.length-1);
+        // mergeSort(nums, 0, nums.length-1);
+        // return nums;
+    }
+
+    // static void mergeSort(int arr[], int left, int right)
+	// {
+	// 	//check if array has more than one element
+	// 	if(left < right)
+	// 	{
+	// 		//find middle element
+	// 		int mid = (left + right) / 2;
+	// 		//sort the left half recursively
+	// 		mergeSort(arr, left, mid); // recursion
+	// 		//sort the right half recursively
+	// 		mergeSort(arr, mid+1, right); // 
+	// 		//merger both sorted half
+	// 		merge(arr, left, mid, right);
+	// 	}
+	// }
+    
+    // //function to merge two sorted arrays
+	// static void merge(int arr[], int left, int mid, int right)
+	// {
+	// 	//size of the left array 
+	// 	int n1 = mid - left + 1;
+	// 	//size of the right array
+	// 	int n2 = right - mid;
+		
+	// 	//two temporary arrays 
+	// 	int L[] = new int[n1];
+	// 	int R[] = new int [n2];
+		
+	// 	//copy the data into the temporary left array
+	// 	for(int i=0; i<n1; i++)
+	// 	{
+	// 		L[i] = arr[left+i];
+	// 	}
+	// 	//copy the data into the new temporary right array
+	// 	for(int j=0; j<n2; j++)
+	// 	{
+	// 		R[j] = arr[mid+1+j];
+	// 	}
+	// 	//initial indices
+	// 	int i = 0;
+	// 	int j = 0;
+	// 	int k = left;
+		
+	// 	//compare and merge
+	// 	while(i < n1 && j < n2)
+	// 	{
+	// 		if(L[i] <= R[j])
+	// 		{
+	// 			arr[k] = L[i];
+	// 			i++;
+	// 		}
+	// 		else
+	// 		{
+	// 			arr[k] = R[j];
+	// 			j++;
+	// 		}
+	// 	   k++;
+	// 	}
+	// 	//copy the remaining elements of the left array
+	// 	while(i < n1)
+	// 	{
+	// 		arr[k] = L[i];
+	// 		i++;
+	// 		k++;
+	// 	}
+	// 	//copy the remaining elements of the right array
+	// 	while(j < n2)
+	// 	{
+	// 		arr[k] = R[j];
+	// 		j++;
+	// 		k++;
+	// 	}
+	// }
+
+
+    private static int[] quickSort(int[] nums, int start, int end) {
+        int pivot = nums[start + new Random().nextInt(end - start + 1)];
+        int i = start, j = end;
+        while (i<=j){
+            while(nums[i]<pivot) i++;
+            while (pivot < nums[j]) j--;
+
+            if(i<=j){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if(i<end) quickSort(nums, i, end);
+        if(j>start) quickSort(nums, start, j);
         return nums;
     }
 
-    static void mergeSort(int arr[], int left, int right)
-	{
-		//check if array has more than one element
-		if(left < right)
-		{
-			//find middle element
-			int mid = (left + right) / 2;
-			//sort the left half recursively
-			mergeSort(arr, left, mid); // recursion
-			//sort the right half recursively
-			mergeSort(arr, mid+1, right); // 
-			//merger both sorted half
-			merge(arr, left, mid, right);
-		}
-	}
-    
-    //function to merge two sorted arrays
-	static void merge(int arr[], int left, int mid, int right)
-	{
-		//size of the left array 
-		int n1 = mid - left + 1;
-		//size of the right array
-		int n2 = right - mid;
-		
-		//two temporary arrays 
-		int L[] = new int[n1];
-		int R[] = new int [n2];
-		
-		//copy the data into the temporary left array
-		for(int i=0; i<n1; i++)
-		{
-			L[i] = arr[left+i];
-		}
-		//copy the data into the new temporary right array
-		for(int j=0; j<n2; j++)
-		{
-			R[j] = arr[mid+1+j];
-		}
-		//initial indices
-		int i = 0;
-		int j = 0;
-		int k = left;
-		
-		//compare and merge
-		while(i < n1 && j < n2)
-		{
-			if(L[i] <= R[j])
-			{
-				arr[k] = L[i];
-				i++;
-			}
-			else
-			{
-				arr[k] = R[j];
-				j++;
-			}
-		   k++;
-		}
-		//copy the remaining elements of the left array
-		while(i < n1)
-		{
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
-		//copy the remaining elements of the right array
-		while(j < n2)
-		{
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
-	}
+    public int[] sortArraySelectionSort(int[] nums) {
+        for(int i=0;i<nums.length; i++){
+            int temp = 0;
+            for(int j=i+1;j<nums.length; j++){
+                if(nums[i] > nums[j]){
+                    temp = nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
+            }    
+        }
+        return nums;
+    }
 }
