cae2ba9f83493629c246b06955c3b8ce4b2205fc
Nikitesh Mehare <meharenikitesh@gmail.com>
2026-09-19 10:17:02 +0530

    Time: 2 ms (59.53%) | Memory: 42.9 MB (78.87%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -3,11 +3,6 @@ class Solution {
         Stack<Character> s1 = new Stack<>();
         Stack<Character> s2 = new Stack<>();
 
-        // if(s.length() != t.length())
-        // {
-        //     return false;
-        // }
-
         for(char ch : s.toCharArray())
         {
             if(ch != '#')
@@ -30,20 +25,20 @@ class Solution {
                 s2.pop();
             } 
         }
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+        // StringBuilder sb1 = new StringBuilder();
+        // StringBuilder sb2 = new StringBuilder();
 
-        while(!s1.isEmpty())
-        {
-            sb1.append(s1.pop());
-        }
-        while(!s2.isEmpty())
-        {
-            sb2.append(s2.pop());
-        }
-        String str1 = sb1.reverse().toString();
-        String str2 = sb2.reverse().toString();
+        // while(!s1.isEmpty())
+        // {
+        //     sb1.append(s1.pop());
+        // }
+        // while(!s2.isEmpty())
+        // {
+        //     sb2.append(s2.pop());
+        // }
+        // String str1 = sb1.reverse().toString();
+        // String str2 = sb2.reverse().toString();
         
-        return str1.equals(str2);
+        return s1.equals(s2);
     }
 }
