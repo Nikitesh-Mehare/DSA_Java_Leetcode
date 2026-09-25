122586736dfa816e8e98299b013b48634a8555d4
Nikitesh Mehare <meharenikitesh@gmail.com>
2026-09-25 10:03:59 +0530

    Time: 23 ms (20.97%) | Memory: 178.3 MB (34.91%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -10,28 +10,56 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        // Step 1: Mark values to be removed using a boolean array for O(1) lookup
-        boolean[] toDelete = new boolean[100001];
-        for (int num : nums) {
-            toDelete[num] = true;
+        // // Step 1: Mark values to be removed using a boolean array for O(1) lookup
+        // boolean[] toDelete = new boolean[100001];
+        // for (int num : nums) {
+        //     toDelete[num] = true;
+        // }
+
+        // // Step 2: Use a dummy node to seamlessly handle head deletion edge cases
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // ListNode current = dummy;
+
+        // // Step 3: Traverse the list and skip nodes present in the removal list
+        // while (current.next != null) {
+        //     if (toDelete[current.next.val]) {
+        //         // Bypass the next node
+        //         current.next = current.next.next;
+        //     } else {
+        //         // Move pointer forward only if we didn't delete a node
+        //         current = current.next;
+        //     }
+        // }
+
+        // return dummy.next;
+
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums)
+        {
+            set.add(num);
         }
 
-        // Step 2: Use a dummy node to seamlessly handle head deletion edge cases
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode current = dummy;
+        while(head != null && set.contains(head.val))
+        {
+            head = head.next;
+        }
 
-        // Step 3: Traverse the list and skip nodes present in the removal list
-        while (current.next != null) {
-            if (toDelete[current.next.val]) {
-                // Bypass the next node
+        ListNode current = head;
+
+        while(current != null && current.next != null)
+        {
+            if(set.contains(current.next.val))
+            {
                 current.next = current.next.next;
-            } else {
-                // Move pointer forward only if we didn't delete a node
+            }
+            else
+            {
                 current = current.next;
             }
         }
-
-        return dummy.next;
+        return head;
     }
 }
